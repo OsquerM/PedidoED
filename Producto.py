906fcf05b32273366b156314caa9edd618e30e75
oscar <obenmar793@g.educaand.es>
2025-04-09 13:09:06 +0200

    documentacion producto

--- Producto.py
+++ Producto.py
@@ -1,106 +1,123 @@
-import ProductoDigital
-   
 class Producto:
-    """_summary_
     """
-    def __init__(self, id_producto: str, nombre: str, precio: float, stock: int):
-        """_summary_
+    Clase que representa un producto en un sistema de inventario.
 
-        :param id_producto: _description_
+    Atributos:
+    - id_producto (str): Identificador único del producto.
+    - nombre (str): Nombre del producto.
+    - precio (float): Precio del producto.
+    - stock (int): Cantidad disponible en inventario del producto.
+    """
+
+    def __init__(self, id_producto: str, nombre: str, precio: float, stock: int):
+        """
+        Inicializa un producto con los parámetros proporcionados.
+
+        :param id_producto: El identificador único del producto.
         :type id_producto: str
-        :param nombre: _description_
+        :param nombre: El nombre del producto.
         :type nombre: str
-        :param precio: _description_
+        :param precio: El precio del producto.
         :type precio: float
-        :param stock: _description_
+        :param stock: La cantidad de unidades disponibles del producto.
         :type stock: int
-        """        
+        """
         self._id_producto = id_producto
         self._nombre = nombre
         self._precio = precio
         self._stock = stock
 
     def get_id_producto(self):
-        """_summary_
+        """
+        Obtiene el identificador del producto.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El identificador único del producto.
+        :rtype: str
         """
         return self._id_producto
 
     def set_id_producto(self, id_producto: str):
-        """_summary_
+        """
+        Establece un nuevo identificador para el producto.
 
-        :param id_producto: _description_
+        :param id_producto: El nuevo identificador del producto.
         :type id_producto: str
         """
         self._id_producto = id_producto
 
     def get_nombre(self):
-        """_summary_
+        """
+        Obtiene el nombre del producto.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El nombre del producto.
+        :rtype: str
         """
         return self._nombre
 
     def set_nombre(self, nombre: str):
-        """_summary_
+        """
+        Establece un nuevo nombre para el producto.
 
-        :param nombre: _description_
+        :param nombre: El nuevo nombre del producto.
         :type nombre: str
         """
         self._nombre = nombre
 
     def get_precio(self):
-        """_summary_
+        """
+        Obtiene el precio del producto.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El precio del producto.
+        :rtype: float
         """
         return self._precio
 
     def set_precio(self, precio: float):
-        """_summary_
+        """
+        Establece un nuevo precio para el producto.
 
-        :param precio: _description_
+        :param precio: El nuevo precio del producto.
         :type precio: float
         """
         self._precio = precio
 
     def get_stock(self):
-        """_summary_
+        """
+        Obtiene la cantidad de unidades disponibles del producto en stock.
 
-        :return: _description_
-        :rtype: _type_
+        :return: La cantidad de unidades del producto.
+        :rtype: int
         """
         return self._stock
 
     def set_stock(self, stock: int):
-        """_summary_
+        """
+        Establece una nueva cantidad de unidades disponibles en stock.
 
-        :param stock: _description_
+        :param stock: La nueva cantidad de unidades en stock.
         :type stock: int
         """
         self._stock = stock
 
     def __str__(self):
-        """_summary_
+        """
+        Devuelve una representación en formato cadena del producto.
 
-        :return: _description_
-        :rtype: _type_
+        :return: Una cadena representando al producto con su id, nombre, precio y stock.
+        :rtype: str
         """
         return f"Producto(id: {self._id_producto}, nombre: {self._nombre}, precio: {self._precio}, stock: {self._stock})"
 
     def actualizar_stock(self, cantidad: int):
-        """_summary_
+        """
+        Actualiza el stock del producto incrementando su cantidad.
 
-        :param cantidad: _description_
+        :param cantidad: La cantidad a añadir al stock.
         :type cantidad: int
         """
         self._stock += cantidad
         print(f"Stock de {self._nombre} actualizado a {self._stock}")
 
-# Listas y Diccionarios
-productos = []
 
+# Lista para almacenar los productos
+productos = []
