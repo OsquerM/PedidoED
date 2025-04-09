ec74933ee1773f2ad523a1dd7b57626783f5db98
oscar <obenmar793@g.educaand.es>
2025-04-09 13:03:21 +0200

    documentacion de reseña

--- Reseña.py
+++ Reseña.py
@@ -1,19 +1,34 @@
 from Producto import Producto
 from Cliente import Cliente
 from ProductoDigital import ProductoDigital
-class Reseña(Producto, Cliente):
-    def __init__(self, id_reseña: str, producto: Producto, cliente: Cliente, comentario: str, puntuacion: int):
-        """_summary_
 
-        :param id_rese: _description_
-        :type id_rese: _type_
-        :param producto: _description_
+class Reseña(Producto, Cliente):
+    """
+    Clase que representa una reseña de un producto hecha por un cliente.
+
+    Hereda de las clases Producto y Cliente.
+
+    Atributos:
+    - id_reseña (str): Identificador único de la reseña.
+    - producto (Producto): El producto sobre el que se realiza la reseña.
+    - cliente (Cliente): El cliente que escribe la reseña.
+    - comentario (str): El comentario que el cliente deja sobre el producto.
+    - puntuacion (int): La puntuación dada al producto (de 1 a 5).
+    """
+
+    def __init__(self, id_reseña: str, producto: Producto, cliente: Cliente, comentario: str, puntuacion: int):
+        """
+        Inicializa una nueva reseña con los parámetros proporcionados.
+
+        :param id_reseña: El identificador único de la reseña.
+        :type id_reseña: str
+        :param producto: El producto sobre el que se realiza la reseña.
         :type producto: Producto
-        :param cliente: _description_
+        :param cliente: El cliente que escribe la reseña.
         :type cliente: Cliente
-        :param comentario: _description_
+        :param comentario: El comentario que el cliente deja sobre el producto.
         :type comentario: str
-        :param puntuacion: _description_
+        :param puntuacion: La puntuación dada al producto (de 1 a 5).
         :type puntuacion: int
         """
         self._id_reseña = id_reseña
@@ -23,86 +38,113 @@ class Reseña(Producto, Cliente):
         self._puntuacion = puntuacion
 
     def get_id_reseña(self):
-        """_summary_
+        """
+        Devuelve el identificador de la reseña.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El identificador de la reseña.
+        :rtype: str
         """
         return self._id_reseña
 
     def set_id_reseña(self, id_reseña: str):
-        """_summary_
+        """
+        Establece un nuevo identificador para la reseña.
+
+        :param id_reseña: El nuevo identificador para la reseña.
+        :type id_reseña: str
         """
         self._id_reseña = id_reseña
 
     def get_producto(self):
-        """_summary_
+        """
+        Devuelve el producto asociado a la reseña.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El producto de la reseña.
+        :rtype: Producto
         """
         return self._producto
 
     def set_producto(self, producto: Producto):
-        """_summary_
+        """
+        Establece el producto para la reseña.
 
-        :param producto: _description_
+        :param producto: El nuevo producto asociado a la reseña.
         :type producto: Producto
         """
         self._producto = producto
 
     def get_cliente(self):
-        """_summary_
+        """
+        Devuelve el cliente que ha realizado la reseña.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El cliente que realizó la reseña.
+        :rtype: Cliente
         """
         return self._cliente
 
     def set_cliente(self, cliente: Cliente):
-        """_summary_
+        """
+        Establece el cliente que ha realizado la reseña.
 
-        :param cliente: _description_
+        :param cliente: El nuevo cliente asociado a la reseña.
         :type cliente: Cliente
         """
         self._cliente = cliente
 
     def get_comentario(self):
-        """_summary_
+        """
+        Devuelve el comentario dejado por el cliente en la reseña.
 
-        :return: _description_
-        :rtype: _type_
+        :return: El comentario de la reseña.
+        :rtype: str
         """
         return self._comentario
 
     def set_comentario(self, comentario: str):
-        """_summary_
+        """
+        Establece el comentario para la reseña.
 
-        :param comentario: _description_
+        :param comentario: El nuevo comentario de la reseña.
         :type comentario: str
         """
         self._comentario = comentario
 
     def get_puntuacion(self):
-        """_summary_
+        """
+        Devuelve la puntuación dada al producto.
 
-        :return: _description_
-        :rtype: _type_
+        :return: La puntuación de la reseña.
+        :rtype: int
         """
         return self._puntuacion
 
     def set_puntuacion(self, puntuacion: int):
-        """_summary_
+        """
+        Establece la puntuación para la reseña.
 
-        :param puntuacion: _description_
+        :param puntuacion: La nueva puntuación de la reseña.
         :type puntuacion: int
         """
         self._puntuacion = puntuacion
 
     def __str__(self):
+        """
+        Devuelve una representación en formato cadena de la reseña.
+
+        :return: Cadena representando la reseña con sus atributos.
+        :rtype: str
+        """
         return f"Reseña(id: {self._id_reseña}, producto: {self._producto.get_nombre()}, cliente: {self._cliente.get_nombre()}, " \
                f"comentario: {self._comentario}, puntuacion: {self._puntuacion})"
 
 # Crear una reseña
-reseña1 = Reseña("R001", ProductoDigital, Cliente, "Excelente Ebook", 5)
+producto_digital = ProductoDigital("PD001", "Ebook de Python", 10.99, 50)
+cliente1 = Cliente("C001", "Juan Pérez", "juanperez@email.com", "Calle Ficticia 123")
+
+reseña1 = Reseña("R001", producto_digital, cliente1, "Excelente Ebook", 5)
+
 reseñas = [reseña1]
+
+# Mostrar la reseña
+for reseña in reseñas:
+    print(reseña)
